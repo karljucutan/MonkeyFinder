c821980ef76e3356eeb8177d7ab5e216ac7d2454
karljucutan <karljucutan@gmail.com>
2025-04-11 17:36:44 +0800

    Set dark theme

---
+++ src/MonkeyFinder/MonkeyFinder.Shared/wwwroot/js/theme.js
@@ -0,0 +1,12 @@
+﻿window.setTheme = function (mode) {
+    const darkModeLink = document.getElementById('dark-mode');
+    const lightModeLink = document.getElementById('light-mode');
+
+    if (mode === 'dark') {
+        darkModeLink.disabled = false;
+        lightModeLink.disabled = true;
+    } else {
+        darkModeLink.disabled = true;
+        lightModeLink.disabled = false;
+    }
+};
