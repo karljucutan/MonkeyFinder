854f4213a7bb47e12bdbdfe02873510b0c7edadb
karljucutan <karljucutan@gmail.com>
2025-04-13 18:44:47 +0800

    * Add theme change in MonkeyRatingPage.xaml. * Remove OnDisappearing in MainPage.xaml as it unsubscribes to the OnThemeChanged when the MonkeyRatingPage got pushed and the mainpage disappears.

--- src/MonkeyFinder/MonkeyFinder/MainPage.xaml.cs
+++ src/MonkeyFinder/MonkeyFinder/MainPage.xaml.cs
@@ -5,9 +5,10 @@ using MonkeyFinder.Shared.States;
 
 namespace MonkeyFinder;
 
-public partial class MainPage : ContentPage
+public partial class MainPage : ContentPage, IDisposable
 {
     private readonly ThemeState _themeState;
+    private bool _disposedValue;
 
     public MainPage(ThemeState themeState)
     {
@@ -37,12 +38,21 @@ public partial class MainPage : ContentPage
                 // Set dark theme status bar appearance
                 StatusBar.SetColor(Microsoft.Maui.Graphics.Color.FromArgb("#444034"));
                 StatusBar.SetStyle(StatusBarStyle.LightContent);
+
+                var app = Application.Current;
+                if (app is not null)
+                    app.UserAppTheme = AppTheme.Dark;
+
             }
             else if (theme == DesignThemeModes.Light)
             {
                 // Set light theme status bar appearance
                 StatusBar.SetColor(Microsoft.Maui.Graphics.Color.FromArgb("#DDAF24"));
                 StatusBar.SetStyle(StatusBarStyle.DarkContent);
+
+                var app = Application.Current;
+                if (app is not null)
+                    app.UserAppTheme = AppTheme.Light;
             }
             else if (theme == DesignThemeModes.System)
             {
@@ -54,21 +64,61 @@ public partial class MainPage : ContentPage
                     // Set dark theme status bar appearance
                     StatusBar.SetColor(Microsoft.Maui.Graphics.Color.FromArgb("#444034"));
                     StatusBar.SetStyle(StatusBarStyle.LightContent);
+
+                    var app = Application.Current;
+                    if (app is not null)
+                        app.UserAppTheme = AppTheme.Dark;
                 }
                 else
                 {
                     // Set light theme status bar appearance
                     StatusBar.SetColor(Microsoft.Maui.Graphics.Color.FromArgb("#DDAF24"));
                     StatusBar.SetStyle(StatusBarStyle.DarkContent);
+
+                    var app = Application.Current;
+                    if (app is not null)
+                        app.UserAppTheme = AppTheme.Light;
                 }
             }
         }
     }
 
-    protected override void OnDisappearing()
+    protected virtual void Dispose(bool disposing)
     {
-        base.OnDisappearing();
-        // Unsubscribe from theme changes to avoid memory leaks
-        _themeState.OnThemeChanged -= OnThemeChangedHandler;
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                // Clean up managed resources (event subscriptions)
+                _themeState.OnThemeChanged -= OnThemeChangedHandler;
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _disposedValue = true;
+        }
     }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~MainPage()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+
+    // Removed as it unsubscribes when the MonkeyRatingPage is pushed to the stack and MainPag will be OnDisappearing
+    //protected override void OnDisappearing()
+    //{
+    //    base.OnDisappearing();
+    //    // Unsubscribe from theme changes to avoid memory leaks
+    //    _themeState.OnThemeChanged -= OnThemeChangedHandler;
+    //}
 }
